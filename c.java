ba9c6c463de3352500c90ac684e98afd51024470
polurumanasa3 <manasap142@gmail.com>
2024-12-04 02:01:10 -0800

    Add files via upload

---
+++ c.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class c
+{
+    public static void main(String args[])
+    {
+        Scanner S=new Scanner(System.in);
+        int a=S.nextInt();
+        if((a&1)==0)
+        System.out.println("even");
+        else
+        {
+         System.out.println("odd");
+        }
+    }
+
+}
+
+        
+        
+    
