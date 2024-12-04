628502931ea763ef66d06b7209cc0a7d2964da83
polurumanasa3 <manasap142@gmail.com>
2024-12-04 02:42:40 -0800

    Add files via upload

---
+++ e.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class e
+{
+    public static void main(String args[])
+    {
+        int n=20;
+        if((n&3)==0)
+        {
+         System.out.println("yes");
+        }
+        else
+        {
+        System.out.println("no");
+        }
+    }
+}
+
+        
+        
+        
+        
+    
