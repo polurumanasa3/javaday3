5b8a1edc3896e07e8dce313656dfa4090b4c45f3
polurumanasa3 <manasap142@gmail.com>
2024-12-04 01:42:00 -0800

    Add files via upload

---
+++ b.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class b
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=2;
+        int c=8;
+        if((a<b)&&(a<c))
+         System.out.println("a is small");
+        else if((b<a)&&(b<c))
+        System.out.println("b is small");
+        else
+        System.out.println("c is small");
+        
+    }
+}
+    
